12c54f54fdf463c16ab4431b455d4a5b1b1fec3b
lonebloodgaming <lonebloodgaming@gmail.com>
2025-12-23 23:30:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+import os
 
+TOKEN = os.getenv("TOKEN")
 ADMIN_ROLE = "Admin"
 
 POINTS_TABLE = {
     1: 15, 2: 12, 3: 10, 4: 8, 5: 6,
     6: 4, 7: 2, 8: 1
 }
+
+
