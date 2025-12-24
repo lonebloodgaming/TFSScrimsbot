84164e351c08c1cd3a212dd7c963b1a1a48404dd
lonebloodgaming <lonebloodgaming@gmail.com>
2025-12-23 23:56:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
-
-TOKEN = os.getenv("MTQ1MzIzNTIwMjQxNDQ4MTU0Mg.GTeQPb.e2-rHYiVXp0ki2_KGS1oHoy5dzaj_NrC2Z2JRM")
+TOKEN = os.getenv("TOKEN")
 ADMIN_ROLE = "Admin"
 
 POINTS_TABLE = {
     1: 15, 2: 12, 3: 10, 4: 8, 5: 6,
     6: 4, 7: 2, 8: 1
 }
-
-
-
